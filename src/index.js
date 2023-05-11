840bd61964b370bac9b5a98e72b20dd59c0895c3
sakusan <sssaku.039@gmail.com>
2023-05-11 11:41:44 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -122,13 +122,13 @@
 // console.log(arr3);
 
 // 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
